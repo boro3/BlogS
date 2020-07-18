b6030e74b493c75ae47a37b0a858eaf0d7c2b8bf
Borche <borosoposki@yahoo.com>
2020-07-18 19:19:32 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Contacts from './components/Contact/Contact'
+import NewPost from './components/Newpost/NewPost'
+import DisplaySinglePost from './components/Showsinglepost/DisplaySinglePost'
+import Search from './components/Search/Search'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='main-container'>
+        <div className="parallax"><Header /></div>
+        <Switch>
+          <Route path={'/search'} component={withRouter(Search)} />
+          <Route path={'/posts/:postId'} component={withRouter(DisplaySinglePost)} />
+          <Route path={'/make-post'} component={NewPost} />
+          <Route path={'/contact'} component={Contacts} />
+          <Route exact path={'/'} component={Home} />
+        </Switch>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
