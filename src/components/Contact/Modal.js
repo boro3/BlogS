d9f7fe968df3eee23aaa30cb39aaccc0f08b2d40
Borche <borosoposki@yahoo.com>
2020-07-18 19:38:44 +0200

    another one

--- src/components/Contact/Modal.js
+++ src/components/Contact/Modal.js
@@ -38,15 +38,6 @@ export default class Modal extends Component {
         this.setState({
             [event.target.name]:event.target.value
         })
-        let a=
-        console.log(a)
-    }
-    clearState=()=>{
-        this.setState({
-            name:'',
-            email:'',
-            content:''
-        })
     }
 
     render() {
@@ -56,7 +47,7 @@ export default class Modal extends Component {
                     <span className="close-modal" onClick={()=>this.props.setOpen(false)}><i className="fas fa-window-close"></i></span>
                     <div className="modal-form">
                         <h1>Contact ME</h1>
-                        <form onSubmit={(event)=>{this.submitHandler(event);this.clearState()}} className='modal-input-form'>
+                        <form onSubmit={(event)=>{this.submitHandler(event)}} className='modal-input-form'>
                             <input type="text" name="name" placeholder="Your Name"  onChange={this.inputChangeHandler}/>
                             <input type="email" name="email" placeholder="Email Address"  onChange={this.inputChangeHandler}/>
                             <textarea name="content" placeholder="Type your Message" onChange={this.inputChangeHandler}></textarea>
