72eba79fb901606bedec5f96271ba47c5af4eddb
Borche <borosoposki@yahoo.com>
2020-07-18 21:04:07 +0200

    Modal functions changes

--- src/components/Contact/Modal.js
+++ src/components/Contact/Modal.js
@@ -3,40 +3,38 @@ import * as emailjs from 'emailjs-com'
 
 export default class Modal extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
 
-        this.state={
-            name:'',
-            email:'',
-            content:''
+        this.state = {
+            name: '',
+            email: '',
+            content: ''
         }
     }
-   submitHandler= (event)=>{
-       event.preventDefault();
-       this.setState({
-        name:'',
-        email:'',
-        content:''
-    })
-    
+    submitHandler = (event)=> {
+        event.preventDefault();
         emailjs
-        .sendForm(
-            'gmail',
-            'template_nBOsaEfz_clone',
-            '.modal-input-form',
-            process.env.REACT_APP_APP_ID
-            
-        )
-        .then(()=>{alert('Thank you for contacting! You will recive answer in shortest time notice.')})
-        .catch(error=>alert('The message was not sent try again later.'+ error))     
-        
-   }
-
-    inputChangeHandler= (event) =>{
+            .sendForm(
+                'gmail',
+                'template_nBOsaEfz_clone',
+                '.modal-input-form',
+                process.env.REACT_APP_APP_ID
+            )
+            .then(() => { alert('Thank you for contacting! You will recive answer in shortest time notice.') ; this.props.setOpen(false) })
+            .catch(error => {alert('The message was not sent try again later.' + error);this.props.setOpen(false)})
 
         this.setState({
-            [event.target.name]:event.target.value
+            name: '',
+            email: '',
+            content: ''
+        })
+    }
+
+    inputChangeHandler = (event) => {
+
+        this.setState({
+            [event.target.name]: event.target.value
         })
     }
 
@@ -44,14 +42,14 @@ export default class Modal extends Component {
         return (
             <div id="myModal" className="modal">
                 <div className="modal-content">
-                    <span className="close-modal" onClick={()=>this.props.setOpen(false)}><i className="fas fa-window-close"></i></span>
+                    <span className="close-modal" onClick={() => this.props.setOpen(false)}><i className="fas fa-window-close"></i></span>
                     <div className="modal-form">
                         <h1>Contact ME</h1>
-                        <form onSubmit={(event)=>{this.submitHandler(event)}} className='modal-input-form'>
-                            <input type="text" name="name" placeholder="Your Name"  onChange={this.inputChangeHandler}/>
-                            <input type="email" name="email" placeholder="Email Address"  onChange={this.inputChangeHandler}/>
-                            <textarea name="content" placeholder="Type your Message" onChange={this.inputChangeHandler}></textarea>
-                            <input type="submit"  />
+                        <form onSubmit={(event) => { this.submitHandler(event) }} className='modal-input-form'>
+                            <input type="text" name="name" placeholder="Your Name" onChange={this.inputChangeHandler} value={this.state.name} />
+                            <input type="email" name="email" placeholder="Email Address" onChange={this.inputChangeHandler} value={this.state.email} />
+                            <textarea name="content" placeholder="Type your Message" onChange={this.inputChangeHandler} value={this.state.content}></textarea>
+                            <input type="submit"></input>
                         </form>
                     </div>
                 </div>
